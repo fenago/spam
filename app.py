247b08b630e41a9409b09254ec1efe7dafc704f6
Dr. Ernesto Lee <socrates73@gmail.com>
2024-02-06 14:37:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def predict_text_with_score(model, text):
     return prediction[0], proba_scores
 
 # Streamlit user interface
-st.title('Text Classification App')
+st.title('Dr Lee Text Classification App')
 user_input = st.text_area("Enter text here:")
 if st.button('Predict'):
     prediction, scores = predict_text_with_score(model, user_input)
